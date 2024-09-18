2750a0bed9b075a9980f1de00b1327fbdf963160
Ahmet E. Dag <adag21044@gmail.com>
2024-09-18 21:03:14 +0300

    finished

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject obstacle;
     public Transform spawnPoint;
     int score = 0;
+    public TextMeshProUGUI scoreText;
+    public GameObject playButton;
+    public GameObject player;
 
     // Start is called before the first frame update
     void Start()
     {
-        GameStart();
+        
     }
 
     IEnumerator SpawnObstacle()
@@ -23,8 +27,17 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    public void IncreaseScore()
+    {
+        score++;
+        scoreText.text = score.ToString();
+    }
+
     public void GameStart()
     {
+        player.SetActive(true);
+        playButton.SetActive(false);
         StartCoroutine(SpawnObstacle());
+        InvokeRepeating("IncreaseScore", 2f, 1f); // Call IncreaseScore method every second
     }
 }
